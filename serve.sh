949db0e2a80f24940cfbcc6e0eca71b9d0537dde
hioikawa <101631407+uminomae@users.noreply.github.com>
2026-02-11 08:25:20 +0900

    Change port to 3001 to avoid conflict

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-echo "🌊 Kesson Space - http://127.0.0.1:3000/"
-python3 -m http.server 3000
+echo "🌊 Kesson Space - http://localhost:3001/"
+python3 -m http.server 3001
